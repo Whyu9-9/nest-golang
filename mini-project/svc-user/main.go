5d56df1be6c0181e0f3180b94e14f34f48f956cc
Wahyu <wahyuivanmahendra@gmail.com>
2023-10-08 03:13:22 +0800

    Update main.go

--- mini-project/svc-user/main.go
+++ mini-project/svc-user/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    // Create an empty array to store user names
+    var users []string
+
+    // Add users to the array
+    users = append(users, "Alice")
+    users = append(users, "Bob")
+    users = append(users, "Charlie")
+
+    // Print the users
+    fmt.Println("Users:", users)
 }
